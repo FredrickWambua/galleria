ab680063adb632768ce4fb44c60cd8d835d49a9b
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-08 18:27:44 +0300

    creates test instance of Photo class

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Photo, Category, Location
 
 # Create your tests here.
+
+class TestPhoto(TestCase):
+    def setUp(self):
+        self.category = Category(name='travel')
+        self.category.save_category()
+
+        self.location = Location(name='dubai')
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Photo))
+
+
