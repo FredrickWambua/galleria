5d9fe13998a07d9037772ef156f63d945dccfb95
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-09 08:43:33 +0300

    uses repath for search url

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 
 urlpatterns=[
@@ -8,6 +8,6 @@ urlpatterns=[
     url('^travel', views.travel, name='travel'),
     url('^culture', views.culture, name = 'culture'),
     url('^sports', views.sports, name = 'sports'),
-    url(r'^search/', views.search, name= 'search'),
+    re_path(r'^search/', views.search, name= 'search'),
     path('photodetails/<int:pk>/',views.view_photo, name='picha'),
 ]
