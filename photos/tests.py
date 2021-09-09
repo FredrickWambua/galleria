fc62d48c5963c5631a0024025b7a5948e436a555
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-09 08:51:49 +0300

    creates  test case for category class

--- photos/tests.py
+++ photos/tests.py
@@ -24,4 +24,28 @@ class TestPhoto(TestCase):
         images = Photo.objects.all()
         self.assertTrue(len(images) == 0)
 
+    def tearDown(self):
+        Photo.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+# Testcase for Category class
+class TestCategory(TestCase):
+    def setUp(self):
+        self.category = Category(name='travel')
+        self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
+
 
